751770f6582333288cb59b3c5c614fa8c861923b
Louis Nguyen <Louis.Nguyen@delv.com>
2024-09-08 21:34:21 +1000

    vite config update for GitHub Pages

--- vite.config.js
+++ vite.config.js
@@ -4,5 +4,5 @@ import react from "@vitejs/plugin-react";
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "https://blue6641.github.io/GovHack2024/",
+  base: "/GovHack2024/",
 });
